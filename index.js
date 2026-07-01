9f875e06f20b65ab6c0a768cdbbbc113e2119313
korii1 <issy4897.owo@gmail.com>
2026-07-01 16:27:43 -0300

    support for multiple announcements

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require("express");
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
+let announcements = [];
+let nextId = 1;
+
+function cleanup() {
+    announcements = announcements.filter(a => a.expiresAt > Date.now());
+}
+
+function addAnnouncement(message) {
+    announcements.push({
+        id: nextId++,
+        message: message,
+        createdAt: Date.now(),
+        expiresAt: Date.now() + 60 * 1000
+    });
+}
+
+setInterval(() => {
+    announcements = announcements.filter(a => a.expiresAt > Date.now());
+}, 30 * 1000);
+
+addAnnouncement("TEST ANNOUNCEMENT");
+
+// PING
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+// GLOBAL MESSAGE
 app.get("/global-message", (req, res) => {
-    res.send("{type=announcement, duration=-1, color=#ff0000}TEST");
+    cleanup();
+    res.json(announcements);
 });
 
+//
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
