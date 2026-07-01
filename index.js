5dff7c596317d753b76985a1f8c9427ef2f43678
korii1 <issy4897.owo@gmail.com>
2026-07-01 16:15:18 -0300

    update server

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.get("/global-message", (req, res) => {
+    res.send("{type=announcement, duration=-1, color=#ff0000}TEST");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
